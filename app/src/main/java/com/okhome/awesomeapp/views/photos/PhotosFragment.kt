7a1ba625b699c9e2763d9d4ba7662204b5e73d7f
Denis Yordan <denis.yordan.p@gmail.com>
2021-10-01 13:02:46 +0700

    implement layout options

--- app/src/main/java/com/okhome/awesomeapp/views/photos/PhotosFragment.kt
+++ app/src/main/java/com/okhome/awesomeapp/views/photos/PhotosFragment.kt
@@ -1,18 +1,20 @@
 package com.okhome.awesomeapp.views.photos
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.paging.ExperimentalPagingApi
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.okhome.awesomeapp.R
 import com.okhome.awesomeapp.databinding.FragmentPhotosBinding
 import com.okhome.awesomeapp.views.photos.adapters.PhotosAdapter
 import com.okhome.awesomeapp.views.photos.adapters.PhotosLoadStateAdapter
+import com.okhome.awesomeapp.views.photos.adapters.ViewType
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -26,6 +28,11 @@ class PhotosFragment : Fragment() {
     private val photoAdapter: PhotosAdapter = PhotosAdapter()
 
     private var isBack = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -55,10 +62,39 @@ class PhotosFragment : Fragment() {
         if (!isBack) loadPhotos()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.main_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.grid_view -> {
+                applyView(ViewType.GRID)
+            }
+            R.id.list_view -> {
+                applyView(ViewType.LIST)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun ViewType.getLayoutManager(): RecyclerView.LayoutManager {
+        return when (this) {
+            ViewType.LIST -> {
+                LinearLayoutManager(requireContext())
+            }
+            ViewType.GRID -> {
+                GridLayoutManager(requireContext(), 2)
+            }
+        }
+    }
+
     private fun setupAdapter() {
         binding?.run {
-            recyclerView.layoutManager = LinearLayoutManager(requireContext())
-            recyclerView.adapter = photoAdapter.withLoadStateFooter(
+            recyclerView.layoutManager = photoAdapter.showViewType.getLayoutManager()
+            recyclerView.adapter = photoAdapter
+            photoAdapter.withLoadStateFooter(
                 PhotosLoadStateAdapter { photoAdapter.retry() }
             )
             photoAdapter.addLoadStateListener {
@@ -67,6 +103,17 @@ class PhotosFragment : Fragment() {
             photoAdapter.onClick = {
                 toDetailPhoto(it)
             }
+        }
+    }
+
+    private fun applyView(viewType: ViewType) {
+        val currentView = photoAdapter.showViewType
+        if (currentView == viewType) return
+
+        binding?.run {
+            photoAdapter.showViewType = viewType
+            recyclerView.layoutManager = viewType.getLayoutManager()
+            recyclerView.adapter = photoAdapter
         }
     }
 
